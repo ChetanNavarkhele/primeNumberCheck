0f8b1ac0ed0890b181211f3b3a7de4b82a7044a0
Chetan Navarkhele <chetannavarkhele@gmail.com>
2023-04-30 11:41:27 +0530

    Work on prime number function

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+let cl = console.log;
+
+const primeNumber = (num) => {
+        if(num === 1){
+                return `Given number ${num} is not a prime number`;
+        }
+        if(num === 2){
+                return `Given number ${num} is a prime number`;
+        }
+        for(let i=2; i<num; i++){
+                if(num%i === 0){
+                        return `Given number ${num} is not a prime number`
+                }
+        }
+        return `Given number ${num} is a prime number`;
+}
+let givenNumber = 1;
+cl(primeNumber(givenNumber));
